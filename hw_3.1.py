526a5f9133f985dba28dfbabc60077bc6c02d2ed
vitaly-goryaynov <diagen3688@gmail.com>
2026-01-19 12:04:28 +0300

    add work number 2

--- hw_3.1.py
+++ hw_3.1.py
@@ -1,4 +1,12 @@
+# 1
+
 class Animal:
+    mame: str
+    view: str
+    age: int
+    weight: int
+    vaccine: bool
+
 
     def __init__(self, name: str, view: str, age: int, weight: int, vaccinate: bool):
         self.name = name
@@ -10,4 +18,48 @@ class Animal:
 
 animal1 = Animal('Burenka', 'cow', 10, 150, True )
 animal2 = Animal('Bobik', 'dog', 5, 30, False )
+
+
+# 2 ---------------------------------------------------------------------------------------
+
+class Artifacts:
+    material: str
+    historical_values: str
+    power: int
+
+    def __init__(self, material: str, historical_values: str, power: int):
+        self.material = material
+        self.historical_values = historical_values
+        self.power = power
+
+
+class Date_Ritual:
+    day: int
+    month: int
+    year: int
+
+    def __init__(self, day: int, month: int, year: int):
+        self.day = day
+        self.month = month
+        self.year = year
+
+
+class Society_member:
+    role: str
+    clothes: str
+    experience: int
+
+    def __init__(self, role: str, clothes: str, experience: int):
+        self.role = role
+        self.clothes = clothes
+        self.experience = experience
+
+
+""" Во время проведения церемоний, класс Члены Общества, выбирает в классе Дата Ритуала 
+точную дату проведения и используют в этот день определенные артефакты, выбирая их
+в классе Артефакты.  
+"""
+
+# 3 -------------------------------------------------------------------------------------------
+
 
