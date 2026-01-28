b16c165cc0a421050d488f51bc49008b7c903b17
vitaly-goryaynov <diagen3688@gmail.com>
2026-01-28 16:58:17 +0300

    add 2 h.w

--- hw_3.2.py
+++ hw_3.2.py
@@ -5,7 +5,7 @@ class Animal:
     view: str
     age: int
 
-    def __init__(self, name, view, age):
+    def __init__(self, name:str, view:str, age:int):
         self.name = name
         self.view = view
         self.age = age
@@ -25,3 +25,30 @@ print(cow1)
 
 # 2
 
+class Book:
+    name: str
+    author: str
+    number_pages: int
+
+    def __init__(self, name:str, author:str, number_pages:int):
+        self.name = name
+        self.author = author
+        self.number_pages = number_pages
+
+
+    def __str__(self):
+        return f"Название книги {self.name}, автор {self.author}, количество страниц {self.number_pages}. "
+
+
+    def open_pages(self, other):
+
+        if other > self.number_pages: return f"Книга не может быть открыта! Нет столько страниц."
+
+        return f"Страница {other} открыта"
+
+
+book1 = Book('Кошки', "Куклачёв", 76)
+
+print(book1)
+print(book1.open_pages(77))
+print(book1.open_pages(27))
